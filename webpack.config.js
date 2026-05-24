eea3457d49ad0fc07490c703296aaccece3549b8
Xavi <187180531+bcxavi@users.noreply.github.com>
2026-05-24 16:56:37 +0200

    Add basic restaurant page styling

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,28 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+
   devtool: "eval-source-map",
+
   devServer: {
     watchFiles: ["./src/template.html"],
   },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
