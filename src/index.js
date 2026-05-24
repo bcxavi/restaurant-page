268e2c09afad497aa2fb9faed9d3d60d29e52176
Xavi <187180531+bcxavi@users.noreply.github.com>
2026-05-24 16:46:38 +0200

    Implement tab navigation logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import loadHome from "./home";
+import loadMenu from "./menu";
+import loadContact from "./contact";
+
+const homeBtn = document.querySelector("#home-btn");
+const menuBtn = document.querySelector("#menu-btn");
+const contactBtn = document.querySelector("#contact-btn");
+const content = document.querySelector("#content");
+
+function clearContent() {
+  content.innerHTML = "";
+}
+
+homeBtn.addEventListener("click", () => {
+  clearContent();
+  loadHome();
+});
+
+menuBtn.addEventListener("click", () => {
+  clearContent();
+  loadMenu();
+});
+
+contactBtn.addEventListener("click", () => {
+  clearContent();
+  loadContact();
+});
 
 loadHome();
